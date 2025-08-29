ac53edb9bbf1c748d7b1456280d9b9aa3565c347
Raunak Kumar <rkjsr2005@gmail.com>
2025-08-29 13:49:45 +0530

    Setting Express - CORS & cookieParser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import connectDB from "./db/index.js"
-import dotenv from "dotenv"
+import dotenv from "dotenv"  //the 
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("MongoDB connection failed!!!", error);
+});
