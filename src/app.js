a6a88519d2e7b560beb88b271b8a4dbf7349201d
Raunak Kumar <rkjsr2005@gmail.com>
2025-08-29 13:55:21 +0530

    JsonSize_Limit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import cookieParser from cookie-parser
 const app = express();
 
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({limit: "16kb"}));
 
-app
 
 export {app}
